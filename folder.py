4eea2b45807ab7574df878779a40ec4a1d1f405b
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-27 15:35:07 +0530

    Update folder.py

--- folder.py
+++ folder.py
@@ -1,6 +1,17 @@
 import os
+import time
+base_path = '/home/kishore/my_files'
 
-directory = '/Plan'
+def remove_files(dir_path, n):
+    all_files = os.listdir(dir_path)
+    now = time.time()
+    n_days = n * 86400
+    for f in all_files:
+        file_path = os.path.join(dir_path, f)
+        if not os.path.isfile(file_path):
+            continue
+        if os.stat(file_path).st_mtime < now - n_days:
+            os.remove(file_path)
+            print("Deleted ", f)
 
-os.system("find " + directory + " -mtime +60 -print")
-os.system("find " + directory + " -mtime +60 -delete")
+remove_files(base_path, 30)
