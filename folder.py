860406827bccf5c8011a74486d0194f0b0196fb3
Gouru Srinivas <46845166+gourusrinivas@users.noreply.github.com>
2022-01-27 14:56:34 +0530

    Update folder.py

--- folder.py
+++ folder.py
@@ -1,1 +1,9 @@
-print("hello world")
+import os, time
+path = "/Plan"
+now = time.time()
+
+for filename in os.listdir(path):
+  filestamp = os.stat(os.path.join(path, filename)).st_mtime
+  filecompare = now - 7 * 86400
+  if filestamp < filecompare:
+    print(filename)
